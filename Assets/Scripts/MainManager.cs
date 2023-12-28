0e53a9cc1d9c53f0bda26a7c9d44110c3d4e624d
Oğuzhan Öksel <oguzhanoksel@hotmail.com>
2023-12-28 21:57:51 +0300

    OOP

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
-    public static MainManager Instance;
+    // public static MainManager Instance;
+
+    public static MainManager Instance { get; private set; }
 
     public Color TeamColor;
 
